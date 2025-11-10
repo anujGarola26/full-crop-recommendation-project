1aa2892ed2435ac5c2049376b7f4685753cb65ea
anujGarola26 <garolaanuj1@gmail.com>
2025-11-10 21:10:43 +0530

    fix: CORS problem

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: process.env.CORS_ORIGIN ||  'http://localhost:3000',
+  origin: process.env.CORS_ORIGIN,
 
   credentials: true // if using cookies/auth
 }));
